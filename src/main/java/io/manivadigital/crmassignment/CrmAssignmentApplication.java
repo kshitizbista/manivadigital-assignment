4d8653409d4413d9b56ea3b025db8ad20b0ca218
kshitizbista <tsetiz.bista@gmail.com>
2020-10-13 10:43:31 +0200

    First commit

---
+++ src/main/java/io/manivadigital/crmassignment/CrmAssignmentApplication.java
@@ -0,0 +1,13 @@
+package io.manivadigital.crmassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/manivadigital/crmassignment/CrmAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.manivadigital.crmassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
